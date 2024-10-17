a462565bc5ff24589aa58e57c8e87889c62adfa1
Rudra <71683445+Rudra0Panda@users.noreply.github.com>
2024-10-17 11:55:20 +0530

    Update gemini.sh

--- gemini.sh
+++ gemini.sh
@@ -9,7 +9,7 @@ input="$*"
 json_payload=$(jq -n --arg text "$input and return response as simple text no markdown or anything and dont use '''" '{"contents":[{"parts":[{"text":$text}]}]}')
 response=$(curl -H 'Content-Type: application/json' \
     -d "$json_payload" \
-    -X POST 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyC0PjI51IXezzgqY5L1Ky7Tjy23VsbyrsY' -s)
+    -X POST 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=<yourapikeyhere>' -s)
 if [ $? -eq 0 ]; then
     echo "$response" | jq -r '.candidates[0].content.parts[0].text'
 else
